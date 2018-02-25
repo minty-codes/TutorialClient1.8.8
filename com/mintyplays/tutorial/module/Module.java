3fbc6f62dbf30d2276fb6475d68789735cf21618
MintyCodesYT <35234818+MintyCodesYT@users.noreply.github.com>
2018-02-25 21:09:16 +0000

    I KEEP FORGETTING TO UPLOAD FILES

--- com/mintyplays/tutorial/module/Module.java
+++ com/mintyplays/tutorial/module/Module.java
@@ -16,6 +16,8 @@ public class Module {
         this.key = key;
         this.category = category;
         toggled = false;
+
+        setup();
     }
 
     public void onEnable() {
@@ -60,4 +62,5 @@ public class Module {
     public void setDisplayName(String displayName) {
         this.displayName = displayName;
     }
+    public void setup() {}
 }
